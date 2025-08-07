e8472c8983db063289370411e1e988c1b2abcd6e
IAM-VIJAY-KUMAR <152658162+IAM-VIJAY-KUMAR@users.noreply.github.com>
2025-08-07 22:32:53 +0530

    Delete day2 directory
    delete message

--- day2/Browser.java
+++
@@ -1,7 +0,0 @@
-package week1.day2;
-
-public class Browser {
-    public static void main(String[] args) {
-        System.out.println("This is my Browser");
-    }
-}
--- day2/Firefox.java
+++
@@ -1,3 +0,0 @@
-package week1.day2;
-
-
